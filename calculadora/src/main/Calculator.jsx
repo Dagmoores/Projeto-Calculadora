4ce525812d0578d9732522790ff77af25e0d04ec
Daniel Augusto Moore da Silva <dagmoores@gmail.com>
2022-11-19 20:27:20 -0300

    finalização da calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -3,8 +3,22 @@ import './Calculator.css'
 
 import Button from '../components/Button'
 import Display from '../components/Display';
+import { clear } from '@testing-library/user-event/dist/clear';
+
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0], 
+    current: 0
+}
+
 
 export default class Calculator extends Component {
+
+
+    state = {...initialState}
 
     constructor(props) {
         super(props)
@@ -15,21 +29,78 @@ export default class Calculator extends Component {
 
 
     clearMemory() {
-        console.log('Limpar')
+        this.setState({...initialState})
     };
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '=' 
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+
+            if (currentOperation === "/") {
+                const result = values[0] / values[1]
+                values[0] = result
+                values[1] = 0
+            }
+            if (currentOperation === "*") {
+                const result = values[0] * values[1]
+                values[0] = result
+                values[1] = 0
+            }
+            if (currentOperation === "+") {
+                const result = values[0] + values[1]
+                values[0] = result
+                values[1] = 0
+            }
+            if (currentOperation === "-") {
+                const result = values[0] - values[1]
+                values[0] = result
+                values[1] = 0
+            }
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals? 0 : 1, 
+                clearDisplay: !equals,
+            })
+
+            
+        }
     };
 
     addDigit(n) {
-        console.log(n)
+        //impedir que o usuario coloque dois pontos no número
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const decimalCondition = this.state.displayValue === "0" && n != "." 
+        const clearDisplay = decimalCondition || this.state.clearDisplay
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        const displayValue = currentValue + n
+
+        this.setState({displayValue, clearDisplay: false, })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     };
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100000000000000}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>
@@ -45,7 +116,7 @@ export default class Calculator extends Component {
                 <Button label="3" click={this.addDigit}/>
                 <Button label="+" click={this.setOperation} operation/>
                 <Button label="0" click={this.addDigit} double/>
-                <Button label="." click={this.setOperation} />
+                <Button label="." click={this.addDigit} />
                 <Button label="=" click={this.setOperation} operation/>
             </div>
         )
